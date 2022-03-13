850d32ac1a176b9fdeab7c5df0ea3fdff3950106
Lorenzo Bianco <lorenzo.bianco22@protonmail.com>
2022-03-13 04:31:35 +0100

    show-speed.lua

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # mpv-show-speed
+
 mpv plugin that shows current playback speed
+
+forked from https://github.com/vflorelle/mpv-scripts
+removing pauses
+
+## speed
+always show current playback speed on the OSD
+
+### usage
+key bind|effect
+--------|------
+`e`     |toggle script on/off
+
+### Installation
+copy/link show-speed.lua into mpv scripts folder
---
+++ show-speed.lua
@@ -0,0 +1,20 @@
+local running = false
+local timer
+
+local function message_speed()
+    local current_speed = mp.get_property("speed")
+    mp.osd_message("Speed: "..("%.2f"):format(math.floor(current_speed * 1000) / 1000), 1)
+end
+
+function show_current_speed()
+    running = not running
+    if running then
+        message_speed()
+        timer = mp.add_periodic_timer(1, message_speed)
+    else
+        mp.osd_message("", 1)
+        timer:stop()
+    end
+end
+
+mp.add_key_binding("g", "show_current_speed", show_current_speed)

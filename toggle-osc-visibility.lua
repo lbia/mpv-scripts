4bf0b0146f376945981fac11c5b29b7dcb8ea085
lbia.xyz <mail@lbia.xyz>
2022-08-10 15:48:33 +0200

    increase wait time

--- toggle-osc-visibility.lua
+++ toggle-osc-visibility.lua
@@ -8,29 +8,36 @@ if options.visibility ~= "never" then
     visibility = true
 end
 
--- do not show message
 function toggle_osc_visibility()
+    visibility = not visibility
+    if visibility == false then
+        mp.command("script-message osc-visibility never")
+        -- mp.command("no-osd script-message osc-visibility never")
+    else
+        mp.command("script-message osc-visibility always")
+        -- mp.command("no-osd script-message osc-visibility always")
+    end
+end
+
+-- do not show message
+function toggle_osc_visibility_no_print()
     level = mp.get_property("osd-level")
     mp.set_property_number("osd-level", 0)
 
-    mp.add_timeout(0.025,
-        function()
-            visibility = not visibility
-            if visibility == false then
-                mp.command("script-message osc-visibility never")
-                -- mp.command("no-osd script-message osc-visibility never")
-            else
-                mp.command("script-message osc-visibility always")
-                -- mp.command("no-osd script-message osc-visibility always")
-            end
-        end
+    wait_time = 0.03
+
+    mp.add_timeout(
+        wait_time,
+        toggle_osc_visibility
     )
 
-    mp.add_timeout(0.050,
+    mp.add_timeout(
+        2 * wait_time,
         function()
             mp.set_property_number("osd-level", level)
         end
     )
 end
 
-mp.add_key_binding("y", "toggle_visibility", toggle_osc_visibility)
+mp.add_key_binding("y", "toggle_visibility", toggle_osc_visibility_no_print)
+-- mp.add_key_binding("y", "toggle_visibility", toggle_osc_visibility)
